ea3ccf700d3f4e9806a8aa7f6251251c9b510679
biankamatos <bibimatos756@gmail.com>
2024-11-26 21:35:24 -0300

    subindo projeto

---
+++ app.js
@@ -0,0 +1,17 @@
+import carros2024 from './tabelacarros.js';
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (requisicao, resposta) => {
+       resposta.status(200).send(carros2024);
+});       
+
+
+// Inicia o servidor na porta 3000:
+app.listen(3000,() => console.log("Servidor Rodando com Sucesso"));
+
+// node app.js
+//localhost:3000/
